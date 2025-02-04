a5b6dbdb4da842cfc87e6acb85c0dfca1c12b9ad
ladylavandula <fatoushisawah@gmail.com>
2025-02-03 23:39:51 -0800

    Initial commit

---
+++ src/main/java/com/lavandula/lavglen/LavenderGlen.java
@@ -0,0 +1,16 @@
+package com.lavandula.lavglen;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LavenderGlen implements ModInitializer {
+	public static final String MOD_ID = "lavglen";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/lavandula/lavglen/LavenderGlenClient.java
@@ -0,0 +1,10 @@
+package com.lavandula.lavglen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LavenderGlenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
