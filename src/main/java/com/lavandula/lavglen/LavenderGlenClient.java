a47ad5d4ad2f625d932f405b80368fb187d297a5
ladylavandula <fatoushisawah@gmail.com>
2025-02-04 15:40:41 -0800

    removed black background on placed block, cutout in client

--- src/main/java/com/lavandula/lavglen/LavenderGlenClient.java
+++ src/main/java/com/lavandula/lavglen/LavenderGlenClient.java
@@ -1,10 +1,14 @@
 package com.lavandula.lavglen;
 
+import com.lavandula.lavglen.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class LavenderGlenClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LAVENDER_FLOWER_BLOCK, RenderLayer.getCutout());
 
     }
 }
